1a6fdea24d16f7c2085a1e1bf0b025fb620edfab
doossee <doossee.me@gmail.com>
2023-04-12 01:22:39 +0500

    cors not works

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    'https://django-server-production-8be3.up.railway.app:4200',
-    'https://django-server-production-8be3.up.railway.app:8000',
+    '*',
 )
 
 CORS_ALLOW_METHODS = [
