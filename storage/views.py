3af7dedc49e5b3d89a6c1623b024346f66e6ed14
doossee <doossee.me@gmail.com>
2023-04-12 01:17:09 +0500

    views.py updated

--- storage/views.py
+++ storage/views.py
@@ -7,34 +7,34 @@ from .serializers import ProductSerializer, ClientSerializer, AdventSerializer, 
 class ProductAPIViewSet(ModelViewSet):
 
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer(queryset, many=True)
+    serializer_class = ProductSerializer
     permission_classes = [IsAdminUser]
 
 
 class ClientAPIViewSet(ModelViewSet):
 
     queryset = Client.objects.all()
-    serializer_class = ClientSerializer(queryset, many=True)
+    serializer_class = ClientSerializer
     permission_classes = [IsAdminUser]
 
 
 class AdventAPIViewSet(ModelViewSet):
 
     queryset = Advent.objects.all()
-    serializer_class = AdventSerializer(queryset, many=True)
+    serializer_class = AdventSerializer
     permission_classes = [IsAdminUser]
 
 
 class ConsumptionAPIViewSet(ModelViewSet):
 
     queryset = Consumption.objects.all()
-    serializer_class = ConsumptionSerializer(queryset, many=True)
+    serializer_class = ConsumptionSerializer
     permission_classes = [IsAdminUser]
 
 class ProfitAPIViewSet(ModelViewSet):
 
     queryset = Profit.objects.all()
-    serializer_class = ProfitSerializer(queryset, many=True)
+    serializer_class = ProfitSerializer
     permission_classes = [IsAdminUser]
 
 
