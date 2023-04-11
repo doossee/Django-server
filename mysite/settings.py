04e8567429ae359f8e3312f68e555d1932b7afba
doossee <doossee.me@gmail.com>
2023-04-12 00:48:23 +0500

    settings.py updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,6 +72,35 @@ REST_FRAMEWORK = {
     ],
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = (
+    'https://django-server-production-8be3.up.railway.app:4200',
+    'https://django-server-production-8be3.up.railway.app:8000',
+)
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
