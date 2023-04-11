9630d076d880ade00013b418bee30038422c6326
doossee <doossee.me@gmail.com>
2023-04-12 01:53:40 +0500

    cors still not working

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,12 +72,12 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
-
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+#CORS_ORIGIN_ALLOW_ALL = True
+#
+#CORS_ORIGIN_WHITELIST = [
+#    'http://localhost:3000',
+#]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
