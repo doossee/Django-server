7d0a3e963f4c65435267e07abb23cbf39de56872
doossee <doossee.me@gmail.com>
2023-04-12 02:40:11 +0500

    cors still not working

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Product, Client, Advent, Consumption, Profit
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
@@ -9,6 +11,13 @@ class ProductAPIViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAdminUser]
+   
+    @action(methods=['post'], detail=False)
+    def create_many(self, request):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
 
 
 class ClientAPIViewSet(ModelViewSet):
@@ -17,12 +26,26 @@ class ClientAPIViewSet(ModelViewSet):
     serializer_class = ClientSerializer
     permission_classes = [IsAdminUser]
 
+    @action(methods=['post'], detail=False)
+    def create_many(self, request):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
+
 
 class AdventAPIViewSet(ModelViewSet):
 
     queryset = Advent.objects.all()
     serializer_class = AdventSerializer
     permission_classes = [IsAdminUser]
+
+    @action(methods=['post'], detail=False)
+    def create_many(self, request):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
 
 
 class ConsumptionAPIViewSet(ModelViewSet):
@@ -31,11 +54,25 @@ class ConsumptionAPIViewSet(ModelViewSet):
     serializer_class = ConsumptionSerializer
     permission_classes = [IsAdminUser]
 
+    @action(methods=['post'], detail=False)
+    def create_many(self, request):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
+
 class ProfitAPIViewSet(ModelViewSet):
 
     queryset = Profit.objects.all()
     serializer_class = ProfitSerializer
     permission_classes = [IsAdminUser]
+
+    @action(methods=['post'], detail=False)
+    def create_many(self, request):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
 
 
    
