3cdba5945bdd08c0782dc782f3af6086f427d35b
doossee <doossee.me@gmail.com>
2023-04-19 18:07:54 +0500

    edited models and serializers

--- storage/serializers.py
+++ storage/serializers.py
@@ -52,6 +52,9 @@ class SingleConsumptionSerializer(serializers.ModelSerializer):
         model = SingleConsumption
         fields = '__all__'
 
+
+    
+
 class ConsumptionListSerializer(serializers.ModelSerializer):
     client = ClientSerializer
     consumption = SingleConsumptionSerializer(many=True)
@@ -65,38 +68,9 @@ class ConsumptionListSerializer(serializers.ModelSerializer):
         consumption_list = ConsumptionList.objects.create(**validated_data)
         for consumption_data in consumptions_data:
             consumption, created = SingleConsumption.objects.get_or_create(**consumption_data)
+            consumption.save()
             consumption_list.consumption.add(consumption)
         return consumption_list
-    
-
-
-"""
-  
-    def create(self, validated_data):
-        products_data = validated_data.pop('products')
-        order = Order.objects.create(**validated_data)
-        for product_data in products_data:
-            product, created = Product.objects.get_or_create(**product_data)
-            order.products.add(product)
-        return order
-
-    def update(self, instance, validated_data):
-        products_data = validated_data.pop('products')
-        instance.customer_name = validated_data.get('customer_name', instance.customer_name)
-        instance.total_amount = validated_data.get('total_amount', instance.total_amount)
-        instance.products.clear()
-        for product_data in products_data:
-            product, created = Product.objects.get_or_create(**product_data)
-            instance.products.add(product)
-        instance.save()
-        return instanceproduct = ProductSerializer
-    amount = serializers.IntegerField(min_value=0)
-    price = serializers.DecimalField(max_digits=9, decimal_places=2, min_value=0)
-    sum = serializers.SerializerMethodField()
-
-    def get_sum(self, obj):
-        return obj.amount * obj.price"""
-
 
 
 class ProfitSerializer(serializers.ModelSerializer):
