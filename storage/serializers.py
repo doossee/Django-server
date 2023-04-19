6df42c3e7a1f8c168e8a5522697a3808334f4a3d
doossee <doossee.me@gmail.com>
2023-04-19 18:16:46 +0500

    edited models and serializers

--- storage/serializers.py
+++ storage/serializers.py
@@ -53,24 +53,39 @@ class SingleConsumptionSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-    
-
 class ConsumptionListSerializer(serializers.ModelSerializer):
-    client = ClientSerializer
+    client = ClientSerializer()
     consumption = SingleConsumptionSerializer(many=True)
-    
+
     class Meta:
         model = ConsumptionList
         fields = '__all__'
 
-    def create(self, validated_data):
-        consumptions_data = validated_data.pop('consumption')
-        consumption_list = ConsumptionList.objects.create(**validated_data)
-        for consumption_data in consumptions_data:
-            consumption, created = SingleConsumption.objects.get_or_create(**consumption_data)
-            consumption.save()
-            consumption_list.consumption.add(consumption)
+    def populate_client(self, client_data):
+        client_serializer = ClientSerializer(data=client_data)
+        client_serializer.is_valid(raise_exception=True)
+        client = client_serializer.populate(client_serializer.validated_data)
+        return client
+
+    def populate_consumption(self, consumption_data):
+        consumption_serializer = SingleConsumptionSerializer(data=consumption_data)
+        consumption_serializer.is_valid(raise_exception=True)
+        consumption = consumption_serializer.populate(consumption_serializer.validated_data)
+        return consumption
+
+    def populate(self, validated_data):
+        client_data = validated_data.pop('client')
+        consumption_data = validated_data.pop('consumption')
+        client = self.populate_client(client_data)
+        consumption = [self.populate_consumption(data) for data in consumption_data]
+        consumption_list = ConsumptionList.objects.create(client=client, **validated_data)
+        consumption_list.consumption.set(consumption)
         return consumption_list
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return self.populate(validated_data)
 
 
 class ProfitSerializer(serializers.ModelSerializer):
